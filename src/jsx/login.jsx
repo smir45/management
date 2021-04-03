ac5d0be3455de9996ee356338a95fb703c843a6d
Samir Mishra <smir.mishra1551@gmail.com>
2021-04-03 14:46:08 +0545

    boxes added

--- src/jsx/login.jsx
+++ src/jsx/login.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import '../css/login.css';
+import React from "react";
+import "../css/login.css";
+import { Link } from "react-router-dom";
+import { AiFillLeftCircle } from "react-icons/ai";
 
-export default function Login(){
-    return(
-        <div className="login-main-container">
-            <div className="elements-container">
-                <div className="left-side"></div>
-                <div className="right-sidee"></div>
-            </div>
+export default function Login() {
+  return (
+    <div className="login-main-container">
+      <div className="elements-container">
+        <div className="navbar-login">
+          <Link to="/" className="login-navbar-link">
+            <AiFillLeftCircle id="backarrow" />
+          </Link>
         </div>
-    );
+        <div className="contents">
+          <div className="left-side"></div>
+          <div className="right-side">
+          <h3 className="title">Please Login to proceed.</h3>
+          <form action="" method="get">
+              <p className="lebel">Email | Username</p>
+              <input type="text" name="username" id="username" className="input-box" />
+              <p className="lebel">Password</p>
+              <input type="password" name="password" id="password" className="input-box" />
+          </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
