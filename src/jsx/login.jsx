6bbf58c9e3846387d61e224b15f20b62a65d29d8
Samir Mishra <smir.mishra1551@gmail.com>
2021-04-03 15:53:45 +0545

    login completed

--- src/jsx/login.jsx
+++ src/jsx/login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
         <div className="contents">
           <div className="left-side"></div>
           <div className="right-side">
-          <h3 className="title">Please Login to continue.</h3>
+          <h3 className="title">Please Login to Continue.</h3>
           <form action="" method="get">
               <p className="lebel">Email | Username</p>
               <input type="text" name="username" id="username" className="input-box" />
