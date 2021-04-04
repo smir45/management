f1e9e53a7e36bae2da2725d658ded71a61c7771b
Samir Mishra <smir.mishra1551@gmail.com>
2021-04-04 13:15:16 +0545

    register jsx and css files created

---
+++ src/jsx/register.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/register.css';
+
+export default function Register(){
+    return(
+        <div className="register-main-container">
+            
+        </div>
+    );
+}
